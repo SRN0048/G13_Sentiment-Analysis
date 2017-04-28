3c7eeb19af22769dd090827b730be3b3a0bb8581
lovetose <tingtongkub5@hotmail.com>
2017-04-29 01:29:36 +0700

    Add files via upload

---
+++ WordEntry.java
@@ -0,0 +1,23 @@
+package FirstTry;
+public class WordEntry {
+  String word;
+  int numAppearances=0;
+  int score = 0;
+  int totalScore = 0;
+  
+  public WordEntry(String text,double score){
+    word = text;
+    totalScore+=score;
+    numAppearances=1;
+  }
+  public void addNewAppearance(double score){
+    totalScore+=score;
+    numAppearances++;
+  }
+  public String getWord(){
+    return word;
+  }
+  public double getAverage(){
+    return (double)totalScore/numAppearances;
+  }
+}
